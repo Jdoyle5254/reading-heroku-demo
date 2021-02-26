ffd6204acdb4cd8052223bde84cf37a85082a989
Jen Doyle <jdoyle5254@users.noreply.github.com>
2021-02-25 20:38:10 -0600

    updated file to use proper db

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require("express");
 
 const mongoose = require("mongoose");
@@ -16,7 +17,17 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+if (process.env.NODE_ENV === "production") {
+  mongoose.connect(process.env.MONGODB_URI,  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+} else {
+  mongoose.connect("mongodb://localhost/reactreadinglist");
+}
+
 
 // Start the API server
 app.listen(PORT, function() {
